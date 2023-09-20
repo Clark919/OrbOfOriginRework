e8f23997516bd333600cae77139da2f2dcd25506
HaXr <haxr@outlook.dk>
2023-09-20 14:45:58 +0200

    Changed naming to include the word "Fork"

--- src/main/java/com/clarkster/originsorbrework/OriginsOrbRework.java
+++ src/main/java/com/clarkster/originsorbrework/OriginsOrbRework.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 
 
 public class OriginsOrbRework implements ModInitializer {
-	public static final String MOD_ID = "originsorbrework";
+	public static final String MOD_ID = "originsorbreworkfork";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
